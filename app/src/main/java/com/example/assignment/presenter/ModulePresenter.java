26d1b12feb34c28f6e65e2a5fced720248ee2d05
Dell <vaishjadhav35@gmail.com>
2020-06-23 00:39:01 +0800

    added interface and singleton class

---
+++ app/src/main/java/com/example/assignment/presenter/ModulePresenter.java
@@ -0,0 +1,107 @@
+package com.testmodule.presenter;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+import com.testmodule.module.Modules;
+import com.testmodule.utilities.Constants;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ModulePresenter {
+
+    private Context context;
+    private moduleListListener moduleListListener;
+
+    public ModulePresenter(Context context, ModulePresenter.moduleListListener moduleListListener) {
+        this.context = context;
+        this.moduleListListener = moduleListListener;
+    }
+
+    public interface moduleListListener{
+
+        void success(List<Modules> modulesList);
+        void error(String tag);
+        void failure(String tag);
+
+        void success(int id, String modules);
+    }
+
+    public void getModuleList(){
+
+        // Initialize a new JsonArrayRequest instance
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                Constants.MODULE_LIST_API,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        Gson gson = new Gson();
+                        if (response.length() >0){
+                            List<Modules> modules = Arrays.asList(gson.fromJson(response.toString(), Modules[].class));
+                            moduleListListener.success(modules);
+                            Log.d("TAG", "onResponse: "+response.toString());
+                        }else {
+                            moduleListListener.error("Response not available");
+                        }
+
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        // Do something when error occurred
+                        moduleListListener.failure(error.getMessage());
+                    }
+                }
+        );
+
+        // Add JsonArrayRequest to the RequestQueue
+        VolleySingleton.getInstance(context).addToRequestQueue(jsonArrayRequest);
+    }
+
+    public void getModuleListDetails(int id){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                Constants.MODULE_DETAILS_API + id, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                if (response != null){
+                    try {
+                        int id = response.getInt("id");
+                        String text = response.getString("text");
+                        moduleListListener.success(id, text);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    moduleListListener.error("Response not available");
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        // Add JsonArrayRequest to the RequestQueue
+        VolleySingleton.getInstance(context).addToRequestQueue(jsonObjectRequest);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/assignment/utilities/Constants.java
@@ -0,0 +1,8 @@
+package com.testmodule.utilities;
+
+public class Constants {
+
+    public final static String MODULE_LIST_API = "https://task.free.beeceptor.com/article";
+    public final static String MODULE_DETAILS_API = "https://task.free.beeceptor.com/article/";
+
+}
